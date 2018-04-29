24ed58f65fca4a19cd2ea780c765bb8a890d55bc
adsantos97 <asantos@cs.loyola.edu>
2018-04-29 17:52:07 -0400

    almost done

--- Arizza-movies.pl
+++ Arizza-movies.pl
@@ -4,7 +4,7 @@ use warnings;
 =begin
 Arizza Santos
 CS451 ASN9
-I picked this language because it is VERY similar to my own pet language;
+I picked this language because it is very similar to my own pet language;
 infact, my pet language (Ruby) was influenced by Perl. I am curious as to how
 Perl differs to Ruby and why Matz Matsumoto did not like this language. He said
 that perl has the "smell of a 'toy lanugage'", which is a "computer programming
@@ -12,28 +12,45 @@ language that is not considered to be suitable or capable for building general
 purpose and high-end software and applications".
 =cut
 
-# purpose: reads in the file and puts each line into an array
-# input: nothing
-# returns: array of movies
-sub process_file
-{
-    my @movies;
-    open(my $fh, "<", "./Movies.txt") or die "Can't open < Movies.txt: $!";
-
-    while (my $line = <$fh>)
-    {
-        chomp $line;
-        push @movies, $line;
-    }
-
-    close $fh;
-    return @movies;
-}
-
 sub main
 {
-    my @movies_array = process_file();
-    
+    my $profit = 0;
+
+    open(my $fh, "<", "./Movies.txt") or die "Can't open < Movies.txt: $!";
+
+    foreach my $line (<$fh>)
+    {
+        chomp $line;
+        my @info = split(/:/, $line); 
+
+        # note: some movies have ":" in their titles
+        if (scalar @info == 4)
+        {
+            my @movie_title = @info[0, 1];
+            my $movie = join(':', @movie_name);
+            print("$movie\n");
+        }
+        else
+        {
+            my $movie = $info[0];
+            print("$movie\n"); 
+        }        
+
+        my $movie_budget = $info[-2]; 
+        my $box_office_gross = $info[-1];
+        my $movie_profit = $box_office_gross - $movie_budget;
+        
+        # determine the highest profit
+        if ($movie_profit > $profit)
+        {
+            $profit = $movie_profit;
+            my $movie_name = $movie;
+        }                
+    }
+
+    print("$movie_name made the highest profit. The profit was \$$profit");
+
+    close $fh;
 }
 
 main();
